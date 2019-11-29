8bd362a2dd538204654596d8061aed4cc72abd42
3SPIN\ingo.schaefer <ingo.schaefer@3spin.de>
2019-11-29 14:07:40 +0100

    Initial commit

---
+++ LabsCenter/LabsCenter/LabsCenter/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using LabsCenter.Services;
+using LabsCenter.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace LabsCenter
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("android=8a2eba08-a41d-4765-9e55-2231026c41e3;" +
+                              "ios={Your iOS App secret here}",
+                              typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
